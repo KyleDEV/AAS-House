fcaf0baf5f33a4704f3d08731ca7cb5404f50966
KyleDEV <kyleme@testme.com>
2024-05-12 13:44:51 +0900

    iframe의 src인 html 파일경로끝에 버전번호 붙이도록 함

---
+++ aas/api/getTemplateVersion.php
@@ -0,0 +1,29 @@
+<?php
+
+/*********
+ * aasInsertIframe.js의 Ajax 요청에 응답하는 GET. 
+ * /aas/pages/tempalte-x/index.html 파일의 최종수정시간을 반환한다.
+ * 이 반환값은 Iframe의 src에 해당하는 html에 버전 파라미터로 쓰일 숫자이다. intex.html?v=xxxxxx
+ *********/
+
+// 템플릿 ID를 GET 파라미터에서 받아옴
+$templateId = isset($_GET['templateId']) ? $_GET['templateId'] : null;
+
+// 템플릿 파일 경로 설정
+$templateFilePath = __DIR__ . "/../aas/pages/template-$templateId/index.html";
+
+// 헤더 설정
+header('Content-Type: application/json');
+
+// 파일의 존재 여부 및 수정 시간 체크해서 존재하면 마지막 수정시간을, 존재하지 않으면 현재시간 반환.
+if (file_exists($templateFilePath))
+{
+    $lastModified = filemtime($templateFilePath);
+}
+else
+{
+    $lastModified = time();
+}
+
+// 결과를 JSON 형식으로 클라이언트에 응답
+echo json_encode(['version' => $lastModified]);
--- aas/pages/aasInsertIframe.js
+++ aas/pages/aasInsertIframe.js
@@ -1,16 +1,30 @@
-document.addEventListener('DOMContentLoaded', function () {
+/************
+ * 사이트의 광고노출 페이지에 삽입되어야할 js 파일.
+ * 페이지에서  모든  광고컨테이너 <div class="aasSpace" 를 찾아 그안에 광고배너 iframe을 삽입해준다.
+ *************/
+
+document.addEventListener('DOMContentLoaded', FillAllAasSpacesWithIframes);
+
+function FillAllAasSpacesWithIframes() {
     // 페이지에서 모든 aasSpace 요소 찾기
     var adSpaces = document.querySelectorAll('.aasSpace');
 
     adSpaces.forEach(function (adContainer) {
-        // .aasSpace의 data-template에 맞는 경로로 src 세팅. 기본값은 template-1 폴더경로
+        // .aasSpace의 data-template에 맞는 경로로 src 설정. 기본값은 template-1 폴더 경로
         var template = adContainer.dataset.template || 'template-1';
-        var iframeSrc = `/aas/pages/${template}/index.html`;
 
-        // iframe HTML 문자열 생성
-        var iframeHTML = `<iframe src="${iframeSrc}" frameborder="0" allowfullscreen="true" style="width: 100%; height: 100%;"></iframe>`;
+        // 서버에 버전 정보를 요청하는 AJAX 호출
+        fetch(`/aas/api/getTemplateVersion.php?templateId=${template}`)
+            .then(response => response.json())
+            .then(data => {
+                // 버전 정보를 받아 iframe의 src에 적용
+                var version = data.version;
+                var iframeSrc = `/aas/pages/${template}/index.html?v=${version}`;
+                var iframeHTML = `<iframe src="${iframeSrc}" frameborder="0" allowfullscreen="true" style="width: 100%; height: 100%;"></iframe>`;
 
-        // 광고 공간에 iframe 삽입
-        adContainer.innerHTML = iframeHTML;
+                // 광고 공간에 iframe 삽입
+                adContainer.innerHTML = iframeHTML;
+            })
+            .catch(error => console.error('Failed to fetch template version:', error));
     });
-});
+}
